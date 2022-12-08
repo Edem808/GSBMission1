dbfdf7c98bd7c334b42326b4460799a409fd3972
808 <113182093+Edem808@users.noreply.github.com>
2022-12-08 14:24:39 +0100

    Add files via upload

---
+++ gsbmvcmission1/views/layout/vue_menu.php
@@ -0,0 +1,29 @@
+<nav class="menuLeft">
+  
+  </nav>
+    <!-- Division pour le sommaire -->
+    <nav class="menuLeft">
+        <ul class="menu-ul">
+        <?php if(isset($_SESSION['prenom'])) { ?>
+			<li class="menu-item">
+				  Visiteur :<br>
+				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom'] ?>
+			</li>
+           
+           <li class="menu-item">
+              <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+           </li>
+
+           <li class="menu-item">
+              <a href="index.php?uc=etatFrais&action=FraisVisiteur" title="Etat de tout les frais par visiteurs">Etat de mes frais</a>
+           </li>
+         
+ 	   <li class="menu-item">
+             <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter"> Déco</a>
+           </li>
+         </ul>
+         
+            <?php } ?>
+    </ul>
+  </nav> 
+<section class="content">
